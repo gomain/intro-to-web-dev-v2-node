e6efff02dce874da95f6544100a0c560d329e981
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-03 16:34:45 +0700

    Use Express

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
-const http = require("http");
-// this doesn't work with node --experimental-modules
-//import * as http from "http"; 
+const express = require('express');
 
-const server = http.createServer(function(req, res) {
+const app = express();
+
+app.get('/', function(req,res) {
   console.log(`user visited ${req.url}`);
-  res.end("hello!");
+  res.end('Welcome to my site!');
+});
+
+app.get('/complement', function(req,res) {
+  console.log(`user visited ${req.url}`);
+  res.end('You look wonderful!');
 });
 
 const port = 3000;
 console.log(`listening on http;//localhost:${port}`);
-server.listen(port);
+app.listen(port);
