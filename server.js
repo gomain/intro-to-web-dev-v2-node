d4347fed7afe0ca915af39fb64ed6d6cb321f6f5
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-03 22:11:12 +0700

    feature: random complements

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const complements = [
+  "You look nice today",
+  "That dress looks nice on you",
+  "Have you been working out?",
+  "You can do hard things",
+  "You've gotten far in this course. You're really smart",
+  "You're programming! How cool is that?",
+  "I'm really proud of you",
+  "You made this",
+  "You've learned a lot of things, and that's pretty hard to do"
+];
+
 app.get('/', function(req,res) {
-  console.log(`user visited ${req.url}`);
-  res.end('Welcome to my site!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/complement', function(req,res) {
-  console.log(`user visited ${req.url}`);
-  res.end('You look wonderful!');
+  res.json({ complement: random(complements) }).end();
 });
+
+function random(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+app.use('/public', express.static('./public'));
 
 const port = 3000;
 console.log(`listening on http;//localhost:${port}`);
 app.listen(port);
+
+console.log(__dirname);
